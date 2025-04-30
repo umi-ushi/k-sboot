4142147df6d4f1a245831be320362965e52abe54
umi-ushi <umiushi.me+dev@gmail.com>
2025-04-30 22:56:22 +0900

    init repo

---
+++ src/main/kotlin/wa/umiushi/k_sboot/KSbootApplication.kt
@@ -0,0 +1,11 @@
+package wa.umiushi.k_sboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KSbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<KSbootApplication>(*args)
+}
---
+++ src/test/kotlin/wa/umiushi/k_sboot/KSbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package wa.umiushi.k_sboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KSbootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
