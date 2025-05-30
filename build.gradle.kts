1ea3c10e3c4aff38721f6ae561852186571e627a
umi-ushi <umiushi.me+dev@gmail.com>
2025-05-30 23:22:37 +0900

    [add] dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
+	kotlin("jvm") version "2.1.21"
 	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.5"
+	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
